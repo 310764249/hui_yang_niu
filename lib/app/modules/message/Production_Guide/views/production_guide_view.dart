dac64fee3e973dc82548030798f5a6ef6fc1cdb3
老嘤 <53335594+310764249@users.noreply.github.com>
2024-08-09 18:49:27 +0800

    添加人工人员姓名改为必填,岗位非必填

--- lib/app/modules/message/Production_Guide/views/production_guide_view.dart
+++ lib/app/modules/message/Production_Guide/views/production_guide_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../controllers/production_puide_controller.dart';
@@ -16,13 +17,29 @@ class ProductionGuideView extends GetView<ProductionGuideController> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.of(context).pop();
+            // Navigator.of(context).pop();
+            Get.back();
           },
         ),
         title: const Text('消息详情'),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.white,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          //任务
+          Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8).r,
+                color: Colors.grey.withOpacity(0.5),
+                child: const Text('任  务'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
